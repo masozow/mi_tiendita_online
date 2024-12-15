50bab7bd8113bfc01a1d180947f166d75ef6c905
masozow <14838605+masozow@users.noreply.github.com>
2024-12-14 21:54:43 -0600

    terminados los queries de marcas

--- backend/database/testQueries.js
+++ backend/database/testQueries.js
@@ -1,7 +1,13 @@
 // import { obtenerTodosProductos,obtenerTodosProductosActivosStockMayorCero } from './entities/productos.js';
 // import {insertarEstado,actualizarEstado, obtenerTodosEstados} from './entities/estados.js';
 // import {obtenerTodosRoles}from './entities/roles.js';
-import {insertarProducto, actualizarProducto} from './entities/productos.js';
+// import { insertarProducto, actualizarProducto } from "./entities/productos.js";
+import {
+  insertarMarca,
+  actualizarMarca,
+  obtenerTodasMarcas,
+} from "./entities/marcas_productos.js";
+
 async function main() {
   try {
     // const resultado = await obtenerTodosProductosActivosStockMayorCero();
@@ -11,10 +17,19 @@ async function main() {
     // const resultado = await obtenerTodosEstados();
     // const resultado = await obtenerTodosRoles();
     // const resultado = await insertarProducto('48795409777','Galaxy a23',18.00,3000.00,4000.00,'',2,1,2);
-    const resultado = await actualizarProducto({nombreProducto:'Galaxy A23'});
-    console.log('Resultado query:', resultado);
+    // const resultado = await actualizarProducto({
+    //   idProducto: 24,
+    //   nombreProducto: "Galaxy A23",
+    // });
+    // const resultado = await insertarMarca("Maggi", 1);
+    // const resultado = await actualizarMarca({
+    //   idMarca: 28,
+    //   nombreMarca: "Maggi",
+    // });
+    const resultado = await obtenerTodasMarcas();
+    console.log("Resultado query:", resultado);
   } catch (err) {
-    console.error('Error en la operación:', err);
+    console.error("Error en la operación:", err);
   }
 }
 main();
