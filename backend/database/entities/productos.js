5514549637633f06ce7141d70021b3811c9451c3
masozow <14838605+masozow@users.noreply.github.com>
2024-12-14 20:26:29 -0600

    terminados los queries de insertar y actualizar productos

--- backend/database/entities/productos.js
+++ backend/database/entities/productos.js
@@ -1,31 +1,151 @@
-import { QueryTypes } from 'sequelize';
-import sequelize from '../config/sequelize.js'; 
+import { QueryTypes } from "sequelize";
+import sequelize from "../config/sequelize.js";
+
+async function insertarProducto(
+  codigoProducto,
+  nombreProducto,
+  stockProducto,
+  costoProducto,
+  precioProducto,
+  fotoProducto = "",
+  idCategoria,
+  idEstado,
+  idMarca
+) {
+  try {
+    const resultado = await sequelize.query(
+      `
+      DECLARE @output_message nvarchar(255)
+      EXEC sp_insertarProducto      @codigo= :codigo, 
+                                    @nombre= :nombre, 
+                                    @stock= :stock, 
+                                    @costo= :costo, 
+                                    @precio= :precio, 
+                                    @foto= :foto, 
+                                    @idCategoria= :idCat, 
+                                    @idEstado= :idEst, 
+                                    @idMarca= :idMar, 
+                                    @message=@output_message OUTPUT
+      SELECT @output_message AS mensaje;
+      `,
+      {
+        replacements: {
+          codigo: codigoProducto,
+          nombre: nombreProducto,
+          stock: stockProducto,
+          costo: costoProducto,
+          precio: precioProducto,
+          foto: fotoProducto,
+          idCat: idCategoria,
+          idEst: idEstado,
+          idMar: idMarca,
+        },
+        type: QueryTypes.SELECT,
+      }
+    );
+    const mensaje = resultado[0]?.mensaje;
+    return mensaje;
+  } catch (err) {
+    console.error("Error al ejecutar el procedimiento:", err);
+    throw err;
+  } finally {
+    await sequelize.close();
+    console.log("Conexión cerrada.");
+  }
+}
+
+async function actualizarProducto({
+  idProducto,
+  codigoProducto = null,
+  nombreProducto = null,
+  stockProducto = null,
+  costoProducto = null,
+  precioProducto = null,
+  fotoProducto = null,
+  idCategoria = null,
+  idEstado = null,
+  idMarca = null,
+} = {}) {
+  try {
+    if (!idProducto) {
+      throw new Error("El id es obligatorio.");
+    }
+    const resultado = await sequelize.query(
+      `
+      DECLARE @output_message nvarchar(255)
+      EXEC sp_actualizarProducto  @id= :id,
+                                  @codigo= :codigo, 
+                                  @nombre= :nombre, 
+                                  @stock= :stock, 
+                                  @costo= :costo, 
+                                  @precio= :precio, 
+                                  @foto= :foto, 
+                                  @idCategoria= :idCat, 
+                                  @idEstado= :idEst, 
+                                  @idMarca= :idMar, 
+                                  @message=@output_message OUTPUT
+      SELECT @output_message AS mensaje;
+      `,
+      {
+        replacements: {
+          id: idProducto,
+          codigo: codigoProducto,
+          nombre: nombreProducto,
+          stock: stockProducto,
+          costo: costoProducto,
+          precio: precioProducto,
+          foto: fotoProducto,
+          idCat: idCategoria,
+          idEst: idEstado,
+          idMar: idMarca,
+        },
+        type: QueryTypes.SELECT,
+      }
+    );
+    const mensaje = resultado[0]?.mensaje;
+    return mensaje;
+  } catch (err) {
+    console.error("Error al ejecutar el procedimiento:", err);
+    throw err;
+  } finally {
+    await sequelize.close();
+    console.log("Conexión cerrada.");
+  }
+}
 
 async function obtenerTodosProductos() {
   try {
-    const productos = await sequelize.query('EXEC sp_obtenerTodosProductos', {
+    const productos = await sequelize.query("EXEC sp_obtenerTodosProductos", {
       type: QueryTypes.SELECT,
     });
     return productos;
   } catch (err) {
-    console.error('Error al ejecutar el procedimiento:', err);
-    throw err; 
+    console.error("Error al ejecutar el procedimiento:", err);
+    throw err;
   } finally {
     await sequelize.close();
-    console.log('Conexión cerrada.');
+    console.log("Conexión cerrada.");
   }
 }
 
 async function obtenerTodosProductosActivosStockMayorCero() {
   try {
-    const datos = await sequelize.query('SELECT * FROM vw_Total_productos_activos_stock_mayor_cero', {
-      type: QueryTypes.SELECT,
-    });
-    console.log('Datos obtenidos de la vista:', datos);
+    const datos = await sequelize.query(
+      "SELECT * FROM vw_Total_productos_activos_stock_mayor_cero",
+      {
+        type: QueryTypes.SELECT,
+      }
+    );
+    console.log("Datos obtenidos de la vista:", datos);
     return datos;
   } catch (err) {
-    console.error('Error al consultar la vista:', err);
+    console.error("Error al consultar la vista:", err);
   }
 }
 
-export { obtenerTodosProductos,obtenerTodosProductosActivosStockMayorCero };
+export {
+  obtenerTodosProductos,
+  obtenerTodosProductosActivosStockMayorCero,
+  insertarProducto,
+  actualizarProducto,
+};
--- backend/database/testQueries.js
+++ backend/database/testQueries.js
@@ -1,6 +1,7 @@
-import { obtenerTodosProductos,obtenerTodosProductosActivosStockMayorCero } from './entities/productos.js';
-import {insertarEstado,actualizarEstado, obtenerTodosEstados} from './entities/estados.js';
-import {obtenerTodosRoles}from './entities/roles.js';
+// import { obtenerTodosProductos,obtenerTodosProductosActivosStockMayorCero } from './entities/productos.js';
+// import {insertarEstado,actualizarEstado, obtenerTodosEstados} from './entities/estados.js';
+// import {obtenerTodosRoles}from './entities/roles.js';
+import {insertarProducto, actualizarProducto} from './entities/productos.js';
 async function main() {
   try {
     // const resultado = await obtenerTodosProductosActivosStockMayorCero();
@@ -8,7 +9,9 @@ async function main() {
     // const resultado = await insertarEstado('Prueba');
     // const resultado = await actualizarEstado(1,'Activo');
     // const resultado = await obtenerTodosEstados();
-    const resultado = await obtenerTodosRoles();
+    // const resultado = await obtenerTodosRoles();
+    // const resultado = await insertarProducto('48795409777','Galaxy a23',18.00,3000.00,4000.00,'',2,1,2);
+    const resultado = await actualizarProducto({nombreProducto:'Galaxy A23'});
     console.log('Resultado query:', resultado);
   } catch (err) {
     console.error('Error en la operación:', err);
