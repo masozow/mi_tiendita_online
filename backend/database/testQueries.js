fe82d67ed668fa2fe67a473e3cf60f51d0602b8c
masozow <14838605+masozow@users.noreply.github.com>
2024-12-16 00:42:41 -0600

    terminados los queries de operadores

--- backend/database/testQueries.js
+++ backend/database/testQueries.js
@@ -5,7 +5,8 @@
 // import {  insertarMarca,  actualizarMarca,  obtenerTodasMarcas,} from "./entities/marcas_productos.js";
 // import { categorias } from "./entities/categorias_productos.js";
 // import { usuarios } from "./entities/usuarios.js";
-import { clientes } from "./entities/clientes.js";
+// import { clientes } from "./entities/clientes.js";
+import { operadores } from "./entities/operadores.js";
 async function main() {
   try {
     // const resultado = await obtenerTodosProductosActivosStockMayorCero();
@@ -58,7 +59,14 @@ async function main() {
     //   nombre: "John Smith",
     // });
     // const resultado = await clientes.obtenerTodo();
-    const resultado = await clientes.obtenerTodoPorID(7);
+    // const resultado = await clientes.obtenerTodoPorID(7);
+    // const resultado = await operadores.insertar(7, 1);
+    // const resultado = await operadores.actualizar({
+    //   idOperador: 8,
+    //   idUsuario: 14,
+    // });
+    // const resultado = await operadores.obtenerTodo();
+    const resultado = await operadores.obtenerTodoPorID(8);
     console.log("Resultado query:", resultado);
   } catch (err) {
     console.error("Error en la operación:", err);
